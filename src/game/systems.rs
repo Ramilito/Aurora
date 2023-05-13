41b5574193d4502dd4e2af682959689f0bb005f8
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-13 09:57:07 +0200

    added sword to map

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -10,7 +10,7 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 
     commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(0.0, 2.0, 0.0),
+        transform: Transform::from_xyz(0.0, 3.0, 0.0),
         point_light: PointLight {
             intensity: 1000.0,
             color: Color::ORANGE,
@@ -19,6 +19,7 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         ..default()
     });
+
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(-5.0, 15.0, 10.0),
         point_light: PointLight {
@@ -40,6 +41,7 @@ pub fn load_assets(
     _my_assets: Res<MyAssets>,
     mut commands: Commands,
     meshes: ResMut<Assets<Mesh>>,
+    asset_server: Res<AssetServer>,
 ) {
     if let Some(tower) = meshes.get(&_my_assets.tower_mesh) {
         let m = Collider::from_bevy_mesh(tower, &ComputedColliderShape::TriMesh).unwrap();
@@ -62,6 +64,16 @@ pub fn load_assets(
                         (0.0_f32).to_radians(),
                     )),
             ));
+
+        commands
+            .spawn(SceneBundle {
+                scene: asset_server.load("models/sword.gltf#Scene0"),
+                ..default()
+            })
+            // .insert(Collider::cuboid(0.25, 0.4, 0.2))
+            // .insert(RigidBody::Dynamic)
+            .insert(TransformBundle::from(
+                Transform::from_xyz(0.0, 2.5, 0.0), // .with_scale(Vec3::new(0.2, 0.2, 0.2)),
+            ));
     }
 }
-
