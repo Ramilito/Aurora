281863b1b96bd8e9e46b2cca02ae9a8da718553a
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-16 20:36:42 +0200

    don't print debug

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -3,18 +3,14 @@ use bevy::prelude::*;
 pub mod components;
 mod systems;
 
-use systems::{load_assets, player_movement};
-
 use crate::components::AppState;
-
-use self::systems::read_result_system;
+use systems::{load_assets, player_movement};
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(load_assets.in_schedule(OnEnter(AppState::InGame)))
-            .add_system(read_result_system)
             .add_system(player_movement);
     }
 }
