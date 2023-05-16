6f28e855a80f8738769626becdaf31453e6b8c6e
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-16 20:37:03 +0200

    use kinematic character controller instead

--- src/player/systems.rs
+++ src/player/systems.rs
@@ -1,11 +1,11 @@
 use crate::components::MyAssets;
-use bevy_rapier3d::prelude::{CharacterLength, KinematicCharacterControllerOutput};
+use bevy_rapier3d::prelude::CharacterLength;
 
 use super::components::Player;
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::{Collider, Dominance, KinematicCharacterController, RigidBody};
+use bevy_rapier3d::prelude::{Collider, KinematicCharacterController, RigidBody};
 
-pub const PLAYER_SPEED: f32 = 3.0;
+pub const PLAYER_SPEED: f32 = 2.0;
 
 #[derive(Bundle)]
 pub struct PlayerBundle {
@@ -27,48 +27,48 @@ pub fn load_assets(_my_assets: Res<MyAssets>, mut commands: Commands) {
     commands
         .spawn(PlayerBundle::new(_my_assets.player.clone()))
         .insert(Collider::cuboid(0.25, 0.4, 0.2))
-        // .insert(ColliderMassProperties::Density(10.0))
         .insert(RigidBody::KinematicPositionBased)
-        .insert(KinematicCharacterController {
-            // snap_to_ground: Some(CharacterLength::Absolute(1.5)),
-            ..default()
-        })
-        // .insert(Restitution::coefficient(0.0))
-        .insert(Dominance::group(10))
+        .insert(KinematicCharacterController { ..default() })
+        // .insert(Dominance::group(10))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 1.0, 18.0)));
 }
 
-pub fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
-    for (entity, output) in controllers.iter() {
-        println!(
-            "Entity {:?} moved by {:?} and touches the ground: {:?}",
-            entity, output.effective_translation, output.grounded
-        );
-    }
-}
+// pub fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
+//     for (entity, output) in controllers.iter() {
+//         println!(
+//             "Entity {:?} moved by {:?} and touches the ground: {:?}",
+//             entity, output.effective_translation, output.grounded
+//         );
+//     }
+// }
 
 pub fn player_movement(
+    time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     mut controllers: Query<&mut KinematicCharacterController>,
 ) {
     for mut controller in controllers.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-            controller.translation = Some(Vec3::new(-0.1, -0.1, 0.1));
+            direction += Vec3::new(-1.0, -1.0, 1.0);
         }
         if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-            controller.translation = Some(Vec3::new(0.1, -0.1, -0.1));
+            direction += Vec3::new(1.0, -1.0, -1.0);
         }
         if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-            controller.translation = Some(Vec3::new(-0.1, -0.1, -0.1));
+            direction += Vec3::new(-1.0, -1.0, -1.0);
         }
         if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-            controller.translation = Some(Vec3::new(0.1, -0.1, 0.1));
+            direction += Vec3::new(1.0, -1.0, 1.0);
         }
 
         if !keyboard_input.any_pressed([KeyCode::Right, KeyCode::Left, KeyCode::Up, KeyCode::Down])
         {
-            controller.translation = Some(Vec3::new(0.0, -0.1, 0.0));
+            direction += Vec3::new(0.0, -1.0, 0.0);
         }
-        controller.snap_to_ground = Some(CharacterLength::Absolute(1.5));
+
+        controller.translation = Some(direction * PLAYER_SPEED * time.delta_seconds());
+        controller.snap_to_ground = Some(CharacterLength::Absolute(0.5));
     }
 }
