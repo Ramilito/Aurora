be8227afa5af57d379b2f63c5e6b89cf774038fa
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-22 22:49:09 +0200

    testing generic collider functeion

--- src/collision/mod.rs
+++ src/collision/mod.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{gltf::GltfMesh, prelude::*};
 use bevy_rapier3d::prelude::*;
 
 pub struct CollisionPlugin;
@@ -8,4 +8,21 @@ impl Plugin for CollisionPlugin {
         app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
             .add_plugin(RapierDebugRenderPlugin::default());
     }
+}
+pub fn get_collider(
+    assets_gltfmesh: Res<Assets<GltfMesh>>,
+    meshes: ResMut<Assets<Mesh>>,
+    mesh_handle: (&String, &Handle<GltfMesh>),
+) {
+    let (name, gltf_mesh) = mesh_handle;
+    if !name.contains("collider") {
+        return;
+    }
+
+    let mesh = assets_gltfmesh.get(gltf_mesh).clone().unwrap();
+    let _collider = Collider::from_bevy_mesh(
+        meshes.get(&mesh.primitives[0].mesh.clone()).unwrap(),
+        &ComputedColliderShape::TriMesh,
+    )
+    .unwrap();
 }
