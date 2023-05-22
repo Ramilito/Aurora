b5f9d34a13e2ff6e008b8217cbc4037d145a7b88
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-23 00:39:01 +0200

    generic add collider function

--- src/collision/mod.rs
+++ src/collision/mod.rs
@@ -1,5 +1,10 @@
-use bevy::{gltf::GltfMesh, prelude::*};
+use bevy::{
+    gltf::{Gltf, GltfMesh},
+    prelude::*,
+};
 use bevy_rapier3d::prelude::*;
+
+use crate::game::loading::MyAssets;
 
 pub struct CollisionPlugin;
 
@@ -9,20 +14,34 @@ impl Plugin for CollisionPlugin {
             .add_plugin(RapierDebugRenderPlugin::default());
     }
 }
-pub fn get_collider(
-    assets_gltfmesh: Res<Assets<GltfMesh>>,
-    meshes: ResMut<Assets<Mesh>>,
-    mesh_handle: (&String, &Handle<GltfMesh>),
-) {
-    let (name, gltf_mesh) = mesh_handle;
-    if !name.contains("collider") {
-        return;
-    }
 
-    let mesh = assets_gltfmesh.get(gltf_mesh).clone().unwrap();
-    let _collider = Collider::from_bevy_mesh(
-        meshes.get(&mesh.primitives[0].mesh.clone()).unwrap(),
-        &ComputedColliderShape::TriMesh,
-    )
-    .unwrap();
+pub fn get_scene_colliders(
+    asset_gltf: &Res<Assets<Gltf>>,
+    mut _my_assets: &ResMut<MyAssets>,
+    assets_gltfmesh: &Res<Assets<GltfMesh>>,
+    meshes: &Res<Assets<Mesh>>,
+) -> Vec<(Collider, Transform)> {
+    let mut result = Vec::new();
+    if let Some(platform) = asset_gltf.get(&_my_assets.platform_gltf) {
+        for mesh_handle in platform.named_meshes.iter() {
+            let (_name, gltf_mesh) = mesh_handle;
+
+            let mesh = assets_gltfmesh.get(gltf_mesh).clone().unwrap();
+            let collider = Collider::from_bevy_mesh(
+                meshes.get(&mesh.primitives[0].mesh.clone()).unwrap(),
+                &ComputedColliderShape::TriMesh,
+            )
+            .unwrap();
+
+            let transform = Transform::from_rotation(Quat::from_euler(
+                EulerRot::XYZ,
+                (-90.0_f32).to_radians(),
+                (0.0_f32).to_radians(),
+                (0.0_f32).to_radians(),
+            ));
+
+            result.push((collider, transform));
+        }
+    }
+    return result;
 }
