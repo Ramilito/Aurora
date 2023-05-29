33da9eb12ace13aa1b069eb22aa96467ce4a171c
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-29 07:40:48 +0200

    added puzzle setup

--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -1,21 +1,20 @@
 use bevy::prelude::*;
-// mod compontents;
-// mod resources;
-// mod puzzles;
+mod components;
+mod state_machine;
 mod systems;
 
 use crate::components::AppState;
 
-use self::systems::{check_puzzle_both, check_puzzle_left, check_puzzle_right, setup, load_assets};
+use self::{
+    state_machine::{solved, unsolved},
+    systems::{load_assets, setup},
+};
 pub struct PuzzlePlugin;
 
 impl Plugin for PuzzlePlugin {
     fn build(&self, app: &mut App) {
-        // app.add_startup_system(setup.in_schedule(OnEnter(AppState::InGamme)));
         app.add_system(setup.in_schedule(OnEnter(AppState::InGame)))
             .add_system(load_assets.in_schedule(OnEnter(AppState::InGame)))
-            .add_system(check_puzzle_right.in_set(OnUpdate(AppState::InGame)))
-            .add_system(check_puzzle_left.in_set(OnUpdate(AppState::InGame)))
-            .add_system(check_puzzle_both.in_set(OnUpdate(AppState::InGame)));
+            .add_systems((solved, unsolved).in_set(OnUpdate(AppState::InGame)));
     }
 }
--- src/puzzles/systems.rs
+++ src/puzzles/systems.rs
@@ -4,52 +4,31 @@ use seldom_state::prelude::StateMachine;
 
 use crate::game::loading::MyAssets;
 
-use super::state_machine::{self, Unsolved};
-
-#[derive(Component)]
-pub struct PuzzleBoxLeft;
-
-#[derive(Component)]
-pub struct PuzzlePlateLeft;
-
-#[derive(Component)]
-pub struct PuzzlePlateRight;
-
-#[derive(Component)]
-pub struct PuzzleBoxRight;
-
-#[derive(Component)]
-pub struct PuzzleLightRight;
-
-#[derive(Component)]
-pub struct PuzzleLightLeft;
-
-#[derive(Component)]
-pub struct Box;
-
-#[derive(Component)]
-pub struct Plate;
-
-#[derive(Component)]
-pub struct Light;
+// use super::state_machine::{self, Unsolved};
+use super::{components::*, state_machine};
 
 #[derive(Bundle)]
-pub struct PuzzleBundle {
-    pub piece: Box,
+pub struct PlateBundle {
     pub plate: Plate,
-    pub ligth: Light,
+    name: Name,
+    #[bundle]
+    scene_bundle: SceneBundle,
     default_state: Unsolved,
     state_machine: StateMachine,
 }
 
-impl PuzzleBundle {
-    pub fn new() -> Self {
-        return PuzzleBundle {
+impl PlateBundle {
+    pub fn new(scene: Handle<Scene>, transform: Transform, name: &str) -> Self {
+        return PlateBundle {
+            name: Name::new(name.to_string()),
             state_machine: state_machine::get_state_machine(),
             default_state: Unsolved,
-            piece: Box,
             plate: Plate,
-            ligth: Light,
+            scene_bundle: SceneBundle {
+                scene,
+                transform: transform.with_scale(Vec3::new(1.5, 2.0, 1.5)),
+                ..default()
+            },
         };
     }
 }
@@ -59,10 +38,10 @@ pub fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut commands: Commands,
 ) {
-
     // commands
     //     .spawn((
-    //         PuzzleBoxLeft,
+    //         Box,
+    //         Name::new("box_left"),
     //         PbrBundle {
     //             mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
     //             material: materials.add(Color::ALICE_BLUE.into()),
@@ -85,125 +64,60 @@ pub fn setup(
     //         },
     //     ))
     //     .insert(Collider::cuboid(0.3, 0.3, 0.3))
-    //     .insert(RigidBody::Dynamic)
     //     .insert(TransformBundle::from(Transform::from_xyz(0.0, 3.0, 14.0)))
+    //     .insert(RigidBody::Dynamic)
     //     .insert(Restitution::coefficient(0.3))
     //     .insert(ColliderMassProperties::Density(2.0));
 }
 
-pub fn load_assets(_my_assets: Res<MyAssets>, mut commands: Commands) {
+pub fn load_assets(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    _my_assets: Res<MyAssets>,
+    mut commands: Commands,
+) {
     commands
         .spawn((
-            PuzzleBundle::new(),
-            SceneBundle {
-                scene: _my_assets.platform.clone(),
-                transform: Transform::from_xyz(2.0, 0.615, 3.0)
-                    .with_scale(Vec3::new(1.5, 2.0, 1.5)),
+            Box,
+            Name::new("right"),
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
+                material: materials.add(Color::ALICE_BLUE.into()),
                 ..default()
             },
         ))
-        .with_children(|children| {
-            for (collider, transform) in _my_assets.platform_colliders.iter() {
-                children
-                    .spawn(RigidBody::Fixed)
-                    .insert(collider.clone())
-                    .insert(TransformBundle::from(transform.clone()));
-            }
-        });
-
-    commands
-        .spawn(SceneBundle {
-            scene: _my_assets.barrel.clone(),
-            ..default()
-        })
+        .insert(Collider::cuboid(0.3, 0.3, 0.3))
         .insert(RigidBody::Dynamic)
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 3.0, 14.0)))
         .insert(Restitution::coefficient(0.3))
-        .insert(ColliderMassProperties::Density(2.0))
-        .with_children(|parent| {
-            for (collider, transform) in _my_assets.barrel_colliders.iter() {
-                parent.spawn((
+        .insert(ColliderMassProperties::Density(1.0));
+
+    commands
+        .spawn(PlateBundle::new(
+            _my_assets.platform.clone(),
+            Transform::from_xyz(2.0, 0.515, 3.0),
+            "right",
+        ))
+        .with_children(|children| {
+            for (collider, transform) in _my_assets.platform_colliders.iter() {
+                children.spawn((
+                    RigidBody::Fixed,
                     collider.clone(),
-                    TransformBundle::from_transform(*transform),
+                    TransformBundle::from(transform.clone()),
                 ));
             }
+            children.spawn((
+                Light,
+                PointLightBundle {
+                    transform: Transform::from_xyz(0.0, 0.715, 0.0),
+                    point_light: PointLight {
+                        intensity: 0.0,
+                        color: Color::ORANGE,
+                        shadows_enabled: true,
+                        ..default()
+                    },
+                    ..default()
+                },
+            ));
         });
-}
-
-pub fn check_puzzle_right(
-    query_plate: Query<&Transform, With<PuzzlePlateRight>>,
-    query_box: Query<&Transform, With<PuzzleBoxRight>>,
-    query_light: Query<Entity, With<PuzzleLightRight>>,
-    mut commands: Commands,
-    mut done: Local<bool>,
-) {
-    let _box = query_box.single();
-    if let Ok(_plate) = query_plate.get_single() {
-        let distance = _plate.translation.distance(_box.translation);
-
-        if distance < 1.0 {
-            if !*done {
-                println!("check puzzle right");
-                commands.spawn((
-                    PuzzleLightRight,
-                    PointLightBundle {
-                        transform: Transform::from_xyz(2.0, 0.715, 3.0),
-                        point_light: PointLight {
-                            intensity: 1000.0,
-                            color: Color::ORANGE,
-                            shadows_enabled: true,
-                            ..default()
-                        },
-                        ..default()
-                    },
-                ));
-                *done = true;
-            }
-        } else {
-            for light in query_light.iter() {
-                commands.entity(light).despawn();
-            }
-
-            println!("not close");
-            *done = false;
-        }
-    }
-}
-
-pub fn check_puzzle_left(
-    query_plate: Query<&Transform, With<PuzzlePlateLeft>>,
-    query_box: Query<&Transform, With<PuzzleBoxLeft>>,
-    mut commands: Commands,
-    mut done: Local<bool>,
-) {
-    let _box = query_box.single();
-    if let Ok(_plate) = query_plate.get_single() {
-        let distance = _plate.translation.distance(_box.translation);
-
-        if distance < 1.0 {
-            println!("check puzzle left");
-            if !*done {
-                commands.spawn((
-                    PuzzleLightLeft,
-                    PointLightBundle {
-                        transform: Transform::from_xyz(2.0, 0.715, 3.0),
-                        point_light: PointLight {
-                            intensity: 1000.0,
-                            color: Color::ORANGE,
-                            shadows_enabled: true,
-                            ..default()
-                        },
-                        ..default()
-                    },
-                ));
-                *done = true;
-            }
-        } else {
-            *done = false;
-        }
-    }
-}
-
-pub fn check_puzzle_both() {
-    // println!("check puzzle both");
 }
