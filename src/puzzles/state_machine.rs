a276ff18e54ecc86f293a7e849f47d89ecd7243c
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-05-29 07:39:28 +0200

    added better state management for puzzle

--- src/puzzles/state_machine.rs
+++ src/puzzles/state_machine.rs
@@ -1,23 +1,14 @@
+use crate::puzzles::components::*;
 use bevy::prelude::*;
 use seldom_state::prelude::*;
 
-use super::systems::{Light, Box, Plate};
-
 pub(crate) fn get_state_machine() -> StateMachine {
-    let plate_pressed = PlatePressed { range: 1.0 };
+    let plate_pressed = PlatePressed { range: 0.5 };
     StateMachine::default()
         .trans::<Unsolved>(plate_pressed, Solved)
         .trans::<Solved>(plate_pressed.not(), Unsolved)
         .set_trans_logging(true)
 }
-
-#[derive(Component, Clone, Reflect)]
-#[component(storage = "SparseSet")]
-pub struct Unsolved;
-
-#[derive(Component, Clone, Reflect)]
-#[component(storage = "SparseSet")]
-pub struct Solved;
 
 #[derive(Clone, Copy, FromReflect, Reflect)]
 pub struct PlatePressed {
@@ -27,34 +18,55 @@ pub struct PlatePressed {
 impl BoolTrigger for PlatePressed {
     type Param<'w, 's> = (
         Query<'w, 's, &'static Transform, With<Box>>,
+        Query<'w, 's, &'static Name, With<Box>>,
+        Query<'w, 's, &'static Name, With<Plate>>,
         Query<'w, 's, &'static Transform, With<Plate>>,
-        Query<'w, 's, &'static Transform, With<Light>>,
     );
     fn trigger(
         &self,
         entity: Entity,
-        (puzzlebox, puzzleplate, puzzlelight): Self::Param<'_, '_>,
+        (piece_pos, piece_name, plate_name, plate_pos): Self::Param<'_, '_>,
     ) -> bool {
-        // if let Ok(enemy_transform) = enemies.get(entity) {
-        //     if let Ok(player_transform) = player.get_single() {
-        //         let distance = enemy_transform.translation.distance(player_transform.translation);
-        //         if distance < 1.0 {
-        //             // next_state.set(AppState::InDialog);
-        //             return true;
-        //         }
-        //     }
-        // }
+        // If the box is on the plate, return true
+        // TODO: Check state before running below code
+        if let Ok(piece_pos) = piece_pos.get_single() {
+            if let Ok(plate_pos) = plate_pos.get(entity) {
+                if piece_pos.translation.distance(plate_pos.translation) < self.range {
+                    if plate_name.get_single().unwrap() == piece_name.get_single().unwrap() {
+                        return true;
+                    }
+                }
+            }
+        }
 
-        // println!("triggered");
         false
     }
 }
 
-pub fn unsolved() {
-    // println!("unsolved");
+pub fn unsolved(
+    unsolved: Query<(Entity, &Unsolved)>,
+    mut light: Query<(&mut PointLight, With<Light>)>,
+) {
+    for (_entity, _unsolved) in &unsolved {
+        if let Ok(mut ligth) = light.get_single_mut() {
+            ligth.0.intensity = 0.0;
+        }
+    }
 }
-pub fn solved(solved: Query<(Entity, &Solved)>) {
+pub fn solved(
+    mut done: Local<bool>,
+    mut light: Query<(&mut PointLight, With<Light>)>,
+    solved: Query<(Entity, &Solved)>,
+) {
     for (_entity, _solved) in &solved {
         println!("solved");
+        if !*done {
+            if let Ok(mut ligth) = light.get_single_mut() {
+                ligth.0.intensity = 1000.0;
+            }
+            *done = true;
+        } else {
+            *done = false;
+        }
     }
 }
