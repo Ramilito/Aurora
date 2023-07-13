d49719776db431a76b3c34aeca88c98eccb8bd09
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-07-13 09:00:41 +0200

    fix panorbit camera, not following player correctly though

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-// use bevy_panorbit_camera::PanOrbitCameraPlugin;
+use bevy_panorbit_camera::PanOrbitCameraPlugin;
 pub mod compontents;
 mod systems;
 
@@ -9,7 +9,8 @@ pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_plugin(PanOrbitCameraPlugin)
-        app.add_systems(Startup, setup).add_systems(Update, sync_player_camera);
+        app.add_plugins(PanOrbitCameraPlugin)
+            .add_systems(Startup, setup)
+            .add_systems(Update, sync_player_camera);
     }
 }
